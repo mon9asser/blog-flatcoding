02bfdcbdda91546bd409587a4408f443cbc0f855
Your Name <you@example.com>
2024-09-27 20:44:06 +0300

    das

--- public/admin/src/components/create-story.js
+++ public/admin/src/components/create-story.js
@@ -120,6 +120,81 @@ class CreateWebStories extends Component {
                     }
                 }
                 break;
+            
+                case 'layout-3':
+                    story.data = {
+                        slide_template:name,
+                        headline: '',
+                        paragraph: '',
+                        text_styles: {
+                            headline: '', 
+                            paragraph: ''
+                        },
+                        link: {
+                            text: "Open",
+                            url: "#",
+                            style: {
+                                bg: 'tomato',
+                                color: '#fff',
+                                other_styles: {  
+                                    marginTop: '15px',
+                                    display: 'block',
+                                    padding: '5px 0px',
+                                    borderRadius: '5px',
+                                }
+                            }
+                        },
+                        media_url: '',
+                        overlay:{ 
+                            position: 'center', 
+                            bg: '#000', 
+                            color: '#fff', 
+                            custom_styles: {} 
+                        },
+                        media_cover: {
+                            url: '', 
+                            poster: ''
+                        }
+                    }
+                    break;
+
+                case 'layout-4':
+                    story.data = {
+                        slide_template:name,
+                        headline: '',
+                        paragraph: '',
+                        text_styles: {
+                            headline: '', 
+                            paragraph: ''
+                        },
+                        link: {
+                            text: "Open",
+                            url: "#",
+                            style: {
+                                bg: 'tomato',
+                                color: '#fff',
+                                other_styles: {  
+                                    marginTop: '15px',
+                                    display: 'block',
+                                    padding: '5px 0px',
+                                    borderRadius: '5px',
+                                }
+                            }
+                        },
+                        media_url: '',
+                        overlay:{ 
+                            position: 'center', 
+                            bg: '#000', 
+                            color: '#fff', 
+                            custom_styles: {} 
+                        },
+                        media_cover: {
+                            url: '', 
+                            poster: ''
+                        }
+                    }
+                    break;
+                
         }
     }
 
@@ -170,6 +245,52 @@ class CreateWebStories extends Component {
         );
     }
 
+    StoryComponents = () => {
+        return (
+            <section className="story-components-data">
+                <h1>Create a new story</h1>
+                <div>
+                    
+                    <div className="story-basic-info">
+                        <b className="story-section-title">Story Basic Informations</b>
+                        <div className="row">
+                            <div className="md-6">
+                               <div className="col-field"> 
+                                    <input placeholder="Title" />
+                               </div>
+                               <div className="col-field"> 
+                               <textarea placeholder="Description"></textarea>
+                               </div>
+                            </div>
+                            <div className="md-6"> 
+                                <div className="col-field"> 
+                                    <input placeholder="Meta Title" />
+                               </div>
+                               <div className="col-field"> 
+                                    <textarea placeholder="Meta Description"></textarea>
+                               </div>
+                                <div className="col-field flexbox-fields"> 
+                                    <label>
+                                        <input type="checkbox" />
+                                        Enable Beside Meta Title
+                                    </label>
+                               </div>
+                               
+                               <div className="col-field flexbox-fields"> 
+                                    <label>
+                                        <input type="checkbox" />
+                                        Allow search engines to show this Article in search results?
+                                    </label>
+                               </div>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+
     render() {
         return (
             <div id="app">
@@ -178,13 +299,11 @@ class CreateWebStories extends Component {
                 <SidebarContainer />
 
                 { this.state.settings != null ? <this.SidebarRightAMPScreens/>: ''}
-
-                <section className="section main-section"> 
-                    <div>
-
-                    </div>
-                </section> 
-
+                {
+                    this.state.settings != null ?
+                    <this.StoryComponents /> : ''
+                }
+                
                  
             </div>
         );
