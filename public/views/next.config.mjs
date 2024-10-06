f8e602a6738c48de0f359e9321ba67bc51c2ffae
Your Name <you@example.com>
2024-10-06 20:48:13 +0300

    updates

--- public/views/next.config.mjs
+++ public/views/next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
           },
           {
             protocol: 'https',
-            hostname: 'media.codedtag.com',
+            hostname: 'media.flatcoding.com',
           },
           {
             protocol: 'https',
