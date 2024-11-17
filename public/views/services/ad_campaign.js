8ede191ca1a97f80ffcd616b8008a8afcd34c06a
Montasser <nexycode@gmail.com>
2024-11-17 14:18:53 +0300

    updates

--- public/views/services/ad_campaign.js
+++ public/views/services/ad_campaign.js
@@ -1,8 +1,12 @@
+"use client"; 
 import { useEffect, useState } from "react"; 
-
 export default function AdCompaignBox({ position, data, classes, settings }) {
-  
-  var class_es = classes == undefined ? '': classes;
+ 
+
+
+
+
+  const combinedClasses = classes ? `ad-box ${classes}` : 'ad-box';
     
   // if no ads in this section so return null 
   if( !data || !data.length ) {
@@ -15,196 +19,59 @@ export default function AdCompaignBox({ position, data, classes, settings }) {
     return null;
   }
   var adsbysite = data[index]; 
-  console.log(adsbysite);
 
+ 
+ 
   
-  return (
-    <><b>Show ads block here</b></>
-  );
-}
-
-
-
-/*
-export default function AdCompaignBox({ position, data, classes, settings }) {
-  if (!data || !data.length) {
-    return null;
+  adsbysite.is_requested = true; 
+  
+  if(window == undefined ) {
+    console.error("SSR is enabled, so client will not work!");
+    return null; 
   }
-
-  const index = data.findIndex((x) => x.position === position);
-  if (index === -1) {
-    return null;
-  }
-
-  var campaignBox = data[index].code; 
-  var props_data = null;
+ 
+  // allow data of code to be readable object
+  var campaignBox = adsbysite.code; 
   try {
-    props_data = JSON.parse(campaignBox);
+    adsbysite.code = JSON.parse(campaignBox)
   } catch (error) {
-    return (
-      <i
-        style={{
-          background: '#f7e4e1',
-          border: '1px solid red',
-          color: 'red',
-          width: '100%',
-          height: '50px',
-          display: 'flex',
-          flexDirection: 'rows',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        Sponsor ad box.
-      </i>
-    );
-  }
-
-  const combinedClasses = classes ? `ad-box ${classes}` : 'ad-box';
-
-  if (props_data.sponser_type === 'object') {
-    
-    const { style, ...otherProps } = props_data.sponser_data;
-
-    // Convert style string to a React-compatible object with camelCase properties
-    const styleObject = {};
-    style.split(';').forEach((item) => {
-      const [property, value] = item.split(':');
-      if (property && value) {
-        const camelCaseProperty = property.trim().replace(/-([a-z])/g, (g) => g[1].toUpperCase());
-        styleObject[camelCaseProperty] = value.trim();
-      }
-    });
-
-    // Extract the last part after the '-' from data-* attributes and map them to Adsense props
-    const dataAttributes = Object.keys(otherProps).reduce((acc, key) => {
-      if (key.startsWith('data-')) {
-        const propKey = key.split('-').pop(); // Extract the last part after '-'
-        acc[propKey] = otherProps[key]; // Map the last slug to the prop
-      }
-      return acc;
-    }, {});
-     
-    return <div className={combinedClasses}>
-      <Adsense style={styleObject} {...dataAttributes} />
-    </div>;
-  }
-
-  return <div className={combinedClasses} dangerouslySetInnerHTML={{ __html: props_data.sponser_data }} />;
-}
-
-*/
-
-
-/*
-
-function AdCompaignBoxOld({ position, data, classes, settings }) {
-    
-  
-      if (!data || !data.length) {
-        return null;
-      }
-    
-      const index = data.findIndex((x) => x.position === position);
-      if (index === -1) {
-        return null;
-      }
-    
-      var campaignBox = data[index].code;
-          campaignBox = `
-            {
-              "sponser_type": "object",
-              "sponser_data": {
-                "style": "display:block; text-align:center;",
-                "data-ad-client": "xxxx",
-                "data-ad-layout": "xxxx",
-                "data-ad-slot": "xxxx",
-                "data-ad-format": "xxxx",
-                "data-full-width-responsive": true
-              }
-            }
-          `
-        var props_data = null;
-      try {
-        props_data = JSON.parse(campaignBox);
-      } catch (error) {
-        return <i style={{background: '#f7e4e1', border: '1px solid red', color: 'red', width:'100%', height: '50px', display:'flex', flexDirection:'rows', justifyContent: 'center', alignItems:'center'}}>Sponsor ad box.</i>; 
-      }
-  
-      const combinedClasses = classes ? `ad-box ${classes}` : 'ad-box';
-  
-      if( props_data.sponser_type == 'object' ) {
-          return <ins class="adsbygoogle" {...props_data.sponser_data}></ins>
-      }
-        
-      return <div className={combinedClasses} dangerouslySetInnerHTML={{ __html: props_data.sponser_data }} />;
-      
-  }
-
-*/
-
-
-/*
-export default function AdCompaignBox({ position, data, classes, settings }) {
-  if (!data || !data.length) {
+    console.error(error)
     return null;
   }
 
-  const index = data.findIndex((x) => x.position === position);
-  if (index === -1) {
+  
+  // dont show ad if it is disabled 
+  if( adsbysite.is_enabled == undefined || adsbysite.is_enabled === false ) {
     return null;
   }
+  
+  
 
-  var campaignBox = data[index].code; 
-  var props_data = null;
-  try {
-    props_data = JSON.parse(campaignBox);
-  } catch (error) {
-    return (
-      <i
-        style={{
-          background: '#f7e4e1',
-          border: '1px solid red',
-          color: 'red',
-          width: '100%',
-          height: '50px',
-          display: 'flex',
-          flexDirection: 'rows',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        Sponsor ad box.
-      </i>
-    );
+  // extract sponsers 
+  var {sponsor_type, sponsor_data } = adsbysite.code;
+  console.log(sponsor_data);
+
+ 
+  console.log("Nooooooooooo++++");
+  if( sponsor_type == undefined || sponsor_data == undefined ) {
+    console.error("sponsor_type and sponsor_data are required to setup ad.")
+    return null; 
   }
+ 
+  console.log(sponsor_type, sponsor_data);
+  
+  // for ads by sponser sponsor_data
 
-  const combinedClasses = classes ? `ad-box ${classes}` : 'ad-box';
+  
 
-  if (props_data.sponser_type === 'object') {
-    const { style, ...otherProps } = props_data.sponser_data;
 
-    // Convert style string to a React-compatible object with camelCase properties
-    const styleObject = {};
-    style.split(';').forEach((item) => {
-      const [property, value] = item.split(':');
-      if (property && value) {
-        const camelCaseProperty = property.trim().replace(/-([a-z])/g, (g) => g[1].toUpperCase());
-        styleObject[camelCaseProperty] = value.trim();
-      }
-    });
-
-    const dataAttributes = Object.keys(otherProps).reduce((acc, key) => {
-      if (key.startsWith('data-')) {
-        acc[key] = otherProps[key];
-      }
-      return acc;
-    }, {});
-
-    return <div className={combinedClasses}><ins className="adsbygoogle" style={styleObject} {...dataAttributes}></ins></div>;
+  // for google adsense
+  if(adsbygoogle == undefined && adsbysite) {
+    console.error("Google AdSense Error: Ads issue it will not appear");
+    return null; 
   }
+ 
 
-  return <div className={combinedClasses} dangerouslySetInnerHTML={{ __html: props_data.sponser_data }} />;
+ 
 }
-*/
-
+ 
