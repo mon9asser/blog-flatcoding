20d376d4a6f95c29dc7f752a7392c4e89e22134f
Your Name <you@example.com>
2025-01-13 21:12:36 +0300

    updates

--- public/views/pages/blog.js
+++ public/views/pages/blog.js
@@ -21,32 +21,62 @@ export default function Blog({upcoming}) {
 
 
 export async function getServerSideProps(context) {
-
     try {
-        // wp-json/wordpress-popular-posts/v1/popular-posts?range=last7days
-        // wp-json/wp/v2/tags
-        // wp-json/wp/v2/posts?per_page=6
-        // => followers
-        // => Header and Footer
-        // => Head Tags
-        // => JSON LD Tags
-        var request = await Helper.sendWPRequest({
-            api: "wp-json/wp/v2/tags",
-            method: "get",
-            data: {} 
-        });
+        // Define the requests
+        const requests = [
+            Helper.sendRequest({
+                api: "tutorials-page/get?post_type=1&page_template=blog",
+                method: "get",
+                data: {}
+            }),
+            Helper.sendWPRequest({
+                api: "wp-json/wordpress-popular-posts/v1/popular-posts?range=last7days",
+                method: "get",
+                data: {}
+            }),
+            Helper.sendWPRequest({
+                api: "wp-json/wp/v2/tags",
+                method: "get",
+                data: {}
+            }),
+            Helper.sendWPRequest({
+                api: "wp-json/wp/v2/posts?per_page=6",
+                method: "get",
+                data: {}
+            })
+        ];
+
+        // Wait for all requests to resolve
+        const responses = await Promise.all(requests);
+
+        // Parse JSON from each response
+        const data = await Promise.all(responses.map(response => response.json()));
+
+        // site url 
+        var settings = data[0].settings.length?data[0].settings[0]: {};
+        if(settings.site_address) {
+            var convertToArray = settings.site_address.split('/');
+            if(convertToArray[convertToArray.length - 1 ] !== '/') {
+                settings.site_address = `${settings.site_address}/`;
+            }
+        }
         
-        if (!request.ok) {
-            throw new Error('Server is offline');
-        }
-          
-        var json = await request.json(); 
-      
+        var meta_title = settings.meta_title + ' ' + settings.beside_post_title;
+        
+        // prepare lists from menu  data:data[0].menus
+       // var nav_left = json.menus?.filter( x=> x.menu_name === "main_menu")
+        //var nav_right = json.menus?.filter( x=> x.menu_name === 'main_nav_right');
+       // var company_links = json.menus?.filter( x=> x.menu_name === "company_nav_links")
+        //var follow_links = json.menus?.filter( x=> x.menu_name === 'follow_nav_links');
+        //var nav_links = json.menus?.filter( x=> x.menu_name === 'tags_nav_links');
+         
+
         return {
-            props: {upcoming: json}
-        }
+            props: { upcoming: {data: settings}} 
+        };
 
     } catch (error) {
+        console.error("Error fetching data:", error);
         context.res.statusCode = 500;
         return { props: { error: 'Server is offline, please try again later.' } };
     }
