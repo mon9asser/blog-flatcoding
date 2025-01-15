4ce5fa56a5c7192193a99960ddb77891b5175613
Your Name <you@example.com>
2025-01-15 21:48:35 +0300

    updates

--- public/views/pages/blog/tag/[tag].js
+++ public/views/pages/blog/tag/[tag].js
@@ -1,5 +1,175 @@
 
-export default function Tag({}) {
-     
-    return <b>Tag here</b>;
+
+import "@/app/theme.css";
+import Head from "next/head";
+import Image from "next/image";
+import parse from 'html-react-parser' 
+import { Helper } from "./../../../services/helper";
+import Header from "./../../../parts/header";
+import Footer from "./../../../parts/footer"; 
+import { ServerOffline } from "./../../../services/components";
+import Script from "next/script";
+import { 
+    TutorialsContent
+} from "./../../../services/components"; 
+
+
+export default function Tag({upcoming}) {
+    console.log(upcoming);
+    return <b>Tag Page</b>;
+}
+
+
+export async function getServerSideProps(context) {
+    try {
+        // Define the requests
+        const requests = [
+            Helper.sendRequest({
+                api: "tutorials-page/get?post_type=1&page_template=blog",
+                method: "get",
+                data: {}
+            }),
+            Helper.sendWPRequest({
+                api: "wp-json/custom/v1/popular-posts", // "wp-json/wordpress-popular-posts/v1/popular-posts?range=last7days",
+                method: "get",
+                data: {}
+            }),
+            Helper.sendWPRequest({
+                api: "wp-json/wp/v2/tags",
+                method: "get",
+                data: {}
+            }),
+            Helper.sendWPRequest({
+                api: "wp-json/custom/v1/latest-posts?tag=javascript&page_number=1",
+                method: "get",
+                data: {}
+            }),
+            Helper.sendWPRequest({
+                api: "wp-json/wp/v2/settings",
+                method: "get",
+                data: {}
+            }),
+            Helper.sendWPRequest({
+                api: "wp-json/wp/v2/categories",
+                method: "get",
+                data: {}
+            }),
+        ];
+
+        // Wait for all requests to resolve
+        const responses = await Promise.all(requests);
+
+        // Parse JSON from each response
+        const data = await Promise.all(responses.map(response => response.json()));
+
+        // 1- Site Settings  
+        var settings = data[0].settings.length?data[0].settings[0]: {};
+        if(settings.site_address) {
+            var convertToArray = settings.site_address.split('/');
+            if(convertToArray[convertToArray.length - 1 ] !== '/') {
+                settings.site_address = `${settings.site_address}/`;
+            }
+        }
+
+        // 2- Blog Settings
+        var blogsettings = data[4];
+        var { 
+            url,
+            use_smilies,
+            timezone,
+            time_format,
+            start_of_week,
+            site_icon,
+            site_logo,
+            show_on_front,
+            page_on_front,
+            page_for_posts,
+            description,
+            language,email,default_post_format, default_ping_status,default_category,date_format,
+            ...blog_settings} = blogsettings;
+
+
+        var meta_title = blog_settings.title + ': ' + blog_settings.homepage_title;
+        
+        
+        // prepare lists from menu  data:data[0].menus
+        var nav_left = data[0].menus?.filter( x=> x.menu_name === "main_menu")
+        var nav_right = data[0].menus?.filter( x=> x.menu_name === 'main_nav_right');
+        var company_links = data[0].menus?.filter( x=> x.menu_name === "company_nav_links")
+        var follow_links = data[0].menus?.filter( x=> x.menu_name === 'follow_nav_links');
+        var nav_links = data[0].menus?.filter( x=> x.menu_name === 'tags_nav_links');
+        
+        // popular posts
+        var popular_posts = data[1];
+        
+        // latest posts 
+        var latest_posts = data[3];
+
+        // tags 
+        var tags = data[2].map(x => {
+            x.link = `${settings.site_address}blog/tag/${x.slug}/`;
+            return {
+                id: x.id, 
+                name: x.name, 
+                link: x.link 
+            };
+        });
+
+        // categories
+        var categories = data[5].map(x => {
+            x.link = `${settings.site_address}blog/category/${x.slug}/`;
+            return {
+                id: x.id, 
+                name: x.name, 
+                link: x.link 
+            };
+        });
+
+        var upcoming = {
+            
+            // Settings
+            meta_title,
+            meta_description: description,
+            default_comment_status: blog_settings.default_comment_status,
+            posts_per_page: blog_settings.posts_per_page,
+            title: blog_settings.title,
+            site_logo: settings.site_logo,
+            site_url: settings.site_address,
+            google_analytics: settings.google_analytics,
+            share_social_buttons: settings.share_social_buttons,
+            subscribe_description:settings.subscribe_description,
+            subscribe_title:settings.subscribe_title,
+
+            // Menus 
+            menus: {
+                nav_left,
+                nav_right,
+                company_links,
+                follow_links,
+                nav_links
+            }, 
+
+            // Popular Posts
+            popular_posts,
+            
+            // Tags 
+            tags, 
+
+            // Categories
+            categories, 
+
+            // Latest posts 
+            latest_posts
+        };
+
+
+        return {
+            props: { upcoming } 
+        };
+
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        context.res.statusCode = 500;
+        return { props: { error: 'Server is offline, please try again later.' } };
+    }
 }
